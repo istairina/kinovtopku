76e621a7ffc29cdb3f1b7e63da716f923593da20
Irina Migunova <ista.irina@gmail.com>
2023-04-06 00:29:16 +0300

    feat: add getch hook and interface for movies

---
+++ src/components/MovieList/interface/IMovie.ts
@@ -0,0 +1,14 @@
+export interface IMovie {
+  id: number;
+  title: string;
+  release: Date;
+  director: string[];
+  screenwriter: string[];
+  operator: string;
+  starring: string[];
+  shortDescription: string;
+  description: string;
+  poster: string;
+  preview: string;
+  screenshots: string[];
+}
