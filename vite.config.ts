706e18041036012d12b2598ec5a4734baf2b4512
Irina Migunova <ista.irina@gmail.com>
2023-04-07 10:55:01 +0300

    fix: vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vite/client" />
 
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import * as path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   resolve: {
